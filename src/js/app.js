b3ffa5d7fb02e15007ac9e8ecd39cb24a1161779
Mariana <marianaongay.01@gmail.com>
2025-07-07 17:16:55 -0600

    Avances

---
+++ src/js/app.js
@@ -0,0 +1,42 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+    crearGaleria();
+
+});
+
+function crearGaleria(){
+    const CANTIDAD_IMAGENES = 16
+    const galeria = document.querySelector('.galeria-imagenes')
+
+    for(let i = 1; i <= CANTIDAD_IMAGENES; i++){
+        const imagen = document.createElement('IMG')
+        imagen.src = `src/img/gallery/full/${i}.jpg`
+        imagen.alt = 'Imagen Galería'
+        
+        //event handler
+
+        imagen.onclick = function(){
+            mostrarImagen(i);
+        }
+
+        galeria.appendChild(imagen);
+    }
+}
+
+function mostrarImagen(i){
+    //generar modal
+
+    const modal = document.createElement('DIV')
+    modal.classList.add('modal')
+    modal.onclick = cerrarModal
+
+    //agregar al html
+    const body = document.querySelector('body')
+    body.appendChild(modal)
+}
+
+function cerrarModal(){
+    const modal = document.querySelector('.modal')
+
+    modal?.remove
+}
